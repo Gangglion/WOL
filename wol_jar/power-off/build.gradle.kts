f018f28a7b359466098c5c6476dc6b2ad2746a5a
SeungHyeob Han <o0tmdguq0o@gmail.com>
2025-09-30 16:10:36 +0900

    [power-off.jar]update : fat jar 생성방법 변경, properties 로드하는 과정 변경

--- wol_jar/power-off/build.gradle.kts
+++ wol_jar/power-off/build.gradle.kts
@@ -3,6 +3,7 @@ import org.gradle.internal.impldep.junit.runner.Version.id
 plugins {
     kotlin("jvm") version "2.2.0"
     id("application")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "com.glion"
@@ -30,16 +31,7 @@ application {
     // main 함수가 있는 진입점 클래스 지정
     mainClass.set("com.glion.MainKt")
 }
-
-// 실행 가능한 Fat JAR 생성 설정
-tasks.withType<Jar> {
-    manifest {
-        attributes["Main-Class"] = application.mainClass.get()
-    }
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    from(sourceSets.main.get().output)
-    dependsOn(configurations.runtimeClasspath)
-    from({
-        configurations.runtimeClasspath.get().files.filter { it.isDirectory || it.name.endsWith("jar") }.map { if (it.isDirectory) it else zipTree(it) }
-    })
+sourceSets["main"].resources {
+    srcDirs(project.rootDir)
+    include("local.properties")
 }
