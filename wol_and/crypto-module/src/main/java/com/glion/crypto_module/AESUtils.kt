9ba8beda973ed7c4dd9d98373515a27da3baf588
SeungHyeob Han <o0tmdguq0o@gmail.com>
2025-10-13 14:05:02 +0900

    [App] update : 암복호화 모듈 변경
    - 변경된 모듈 최신화

--- wol_and/crypto-module/src/main/java/com/glion/crypto_module/AESUtils.kt
+++ wol_and/crypto-module/src/main/java/com/glion/crypto_module/AESUtils.kt
@@ -8,45 +8,47 @@ import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
 import javax.crypto.spec.GCMParameterSpec
+import javax.crypto.spec.SecretKeySpec
 
 /**
  * Project : CryptoModuleSample
  * File : AESUtils
- * Created by glion on 2025-09-19
+ * Created by glion on 2025-10-13
  *
  * Description:
- * - AES 키 생성 및 암복호화 Utils(KeyStore 사용)
+ * - AES 키 생성 및 암복호화 Utils(키 생성은 keyStore 로 진행)
  *
  * Copyright @2025 Gangglion. All rights reserved
  */
-object AESUtils {
-    private const val ANDROID_KEYSTORE = "AndroidKeyStore"
-    private const val AES_ALIAS = "aes_key"
-
-    private const val TRANSFORMATION = "AES/GCM/NoPadding"
-    private const val KEY_SIZE = 256
-    private const val IV_SIZE = 12 // GCM 권장값
-    private const val TAG_SIZE = 128
+class AESUtils {
+    companion object {
+        private const val ANDROID_KEYSTORE = "AndroidKeyStore"
+        private const val AES_ALIAS = "aes_key"
+        private const val ALGORITHM = "AES"
+        private const val TRANSFORMATION = "AES/GCM/NoPadding"
+        private const val KEY_SIZE = 256
+        private const val IV_SIZE = 12 // GCM 권장값
+        private const val TAG_SIZE = 128
+    }
 
     private val keystore: KeyStore by lazy {
         KeyStore.getInstance(ANDROID_KEYSTORE).apply { load(null) }
     }
 
-    // aes 키 값 - KeyStore 로 생성한 AES 키는 꺼낼 수 없음
-    var aesKey: SecretKey? = null
-        private set
-
     /**
      * AES 키 생성 - KeyStore 에 저장
      */
-    fun getOrCreateAESKey() {
+    fun getOrCreateAESKey() : SecretKey {
         // 키스토어에 있는지 확인
         if(keystore.containsAlias(AES_ALIAS)) {
             val entry = keystore.getEntry(AES_ALIAS, null) as? KeyStore.SecretKeyEntry
-            aesKey = entry?.secretKey
+            return entry!!.secretKey
         } else {
             // 없으면 생성 후 저장
-            val keygen = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, ANDROID_KEYSTORE)
+            val keygen = KeyGenerator.getInstance(
+                KeyProperties.KEY_ALGORITHM_AES,
+                ANDROID_KEYSTORE
+            )
             val spec = KeyGenParameterSpec.Builder(AES_ALIAS, KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
                 .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
                 .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
@@ -54,37 +56,115 @@ object AESUtils {
                 .setKeySize(KEY_SIZE)
                 .build()
             keygen.init(spec)
-            aesKey = keygen.generateKey()
+            return keygen.generateKey() // 생성과 동시에 KeyStore 에 저장됨
         }
     }
 
+    private fun generateIv() : ByteArray = ByteArray(IV_SIZE).also { SecureRandom().nextBytes(it) }
+
     /**
-     * 암호화
-     * @param origin 암호화하고자 하는 값
+     * KeyStore AES 키로 암호화
+     * @param aesKey KeyStore 에서 생성한 AES 키
+     * @param origin 암호화하고자 하는 값(ByteArray)
      * @return Pair(암호화된 값, iv)
      */
-    fun encrypt(origin: String): Pair<ByteArray, ByteArray> {
+    fun encrypt(aesKey: SecretKey, origin: ByteArray): Pair<ByteArray, ByteArray> {
         val cipher = Cipher.getInstance(TRANSFORMATION)
         cipher.init(Cipher.ENCRYPT_MODE, aesKey)
-        val iv = cipher.iv // cipher 에서 자동 생성해준 iv 이용
-        val encrypted = cipher.doFinal(origin.toByteArray(Charsets.UTF_8))
+        // cipher 에서 자동 생성해준 iv 사용
+        val iv = cipher.iv
+        val encrypted = cipher.doFinal(origin)
 
         return encrypted to iv
     }
 
     /**
-     * 복호화
+     * KeyStore AES 키로 암호화
+     * @param aesKey KeyStore 에서 생성한 AES 키
+     * @param origin 암호화하고자 하는 값(String)
+     * @return Pair(암호화된 값, iv)
+     */
+    fun encrypt(aesKey: SecretKey, origin: String): Pair<ByteArray, ByteArray> {
+        return encrypt(aesKey, origin.toByteArray(Charsets.UTF_8))
+    }
+
+    /**
+     * 외부 AES 키로 암호화
+     * @param aesKey 외부에서 받은 AES 키
+     * @param origin 암호화하고자 하는 값(ByteArray)
+     * @return Pair(암호화된 값, iv)
+     */
+    fun encrypt(aesKey: ByteArray, origin: ByteArray): Pair<ByteArray, ByteArray> {
+        val cipher = Cipher.getInstance(TRANSFORMATION)
+        val secretKey = SecretKeySpec(aesKey, ALGORITHM)
+        val iv = generateIv()
+        val spec = GCMParameterSpec(TAG_SIZE, iv)
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, spec)
+        val encrypted = cipher.doFinal(origin)
+
+        return encrypted to iv
+    }
+
+    /**
+     * 외부 AES 키로 암호화
+     * @param aesKey 외부에서 받은 AES 키
+     * @param origin 암호화하고자 하는 값(String)
+     * @return Pair(암호화된 값, iv)
+     */
+    fun encrypt(aesKey: ByteArray, origin: String): Pair<ByteArray, ByteArray> {
+        return encrypt(aesKey, origin.toByteArray(Charsets.UTF_8))
+    }
+
+    /**
+     * KeyStore AES 키로 복호화
+     * @param aesKey KeyStore 에서 생성한 AES 키
      * @param encrypted 암호화된 값
      * @param iv 암호화 시 사용된 iv 값
-     * @return 복호화된 문자열
+     * @return 복호화된 ByteArray
      */
-    fun decrypt(encrypted: ByteArray, iv: ByteArray) : String {
+    fun decryptToByteArray(aesKey: SecretKey, encrypted: ByteArray, iv: ByteArray) : ByteArray {
         val cipher = Cipher.getInstance(TRANSFORMATION)
         val spec = GCMParameterSpec(TAG_SIZE, iv)
         cipher.init(Cipher.DECRYPT_MODE, aesKey, spec)
 
-        val decryptedValue = cipher.doFinal(encrypted)
+        return cipher.doFinal(encrypted)
+    }
 
-        return String(decryptedValue, Charsets.UTF_8)
+    /**
+     * KeyStore AES 키로 복호화
+     * @param aesKey KeyStore 에서 생성한 AES 키
+     * @param encrypted 암호화된 값
+     * @param iv 암호화 시 사용된 iv 값
+     * @return 복호화된 문자열
+     */
+    fun decryptToString(aesKey: SecretKey, encrypted: ByteArray, iv: ByteArray) : String {
+        return String(decryptToByteArray(aesKey, encrypted, iv), Charsets.UTF_8)
+    }
+
+    /**
+     * 외부 AES 키로 복호화
+     * @param aesKey 외부에서 받은 AES 키
+     * @param encrypted 암호화된 값
+     * @param iv 암호화 시 사용된 iv 값
+     * @return 복호화된 ByteArray
+     */
+    fun decryptToByteArray(aesKey: ByteArray, encrypted: ByteArray, iv: ByteArray) : ByteArray {
+        val cipher = Cipher.getInstance(TRANSFORMATION)
+        val secretKey = SecretKeySpec(aesKey, ALGORITHM)
+        val spec = GCMParameterSpec(TAG_SIZE, iv)
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, spec)
+
+        return cipher.doFinal(encrypted)
+    }
+
+    /**
+     * 외부 AES 키로 복호화
+     * @param aesKey 외부에서 받은 AES 키
+     * @param encrypted 암호화된 값
+     * @param iv 암호화 시 사용된 iv 값
+     * @return 복호화된 문자열
+     */
+    fun decryptToString(aesKey: ByteArray, encrypted: ByteArray, iv: ByteArray) : String {
+        return String(decryptToByteArray(aesKey, encrypted, iv), Charsets.UTF_8)
     }
 }
--- wol_and/crypto-module/src/main/java/com/glion/crypto_module/RSAUtils.kt
+++ wol_and/crypto-module/src/main/java/com/glion/crypto_module/RSAUtils.kt
@@ -5,41 +5,41 @@ import android.security.keystore.KeyProperties
 import java.security.KeyPair
 import java.security.KeyPairGenerator
 import java.security.KeyStore
+import java.security.PrivateKey
+import java.security.PublicKey
 import javax.crypto.Cipher
 
 /**
- * Project : CryptoModule
+ * Project : CryptoModuleSample
  * File : RSAUtils
- * Created by glion on 2025-09-19
+ * Created by glion on 2025-10-13
  *
  * Description:
  * - RSA 키 생성 암복호화 Utils
  *
  * Copyright @2025 Gangglion. All rights reserved
  */
-object RSAUtils {
-    private const val ANDROID_KEYSTORE = "AndroidKeyStore"
-    private const val RSA_ALIAS = "rsa_key"
-
-    private const val RSA_TRANSFORMATION = "RSA/ECB/PKCS1Padding"
-    private const val KEY_SIZE = 2048
+class RSAUtils {
+    companion object {
+        private const val ANDROID_KEYSTORE = "AndroidKeyStore"
+        private const val RSA_ALIAS = "rsa_key"
+        private const val KEY_SIZE = 2048
+        private const val RSA_TRANSFORMATION = "RSA/ECB/PKCS1Padding"
+    }
 
     private val keystore: KeyStore by lazy {
         KeyStore.getInstance(ANDROID_KEYSTORE).apply { load(null) }
     }
 
-    var rsaKey: KeyPair? = null
-        private set
-
-    /**
-     * RSA 키 가져와서 저장, 없으면 생성
-     */
-    fun getOrCreateRSAKeyPair() {
+    fun getOrCreateRSAKeyPair() : KeyPair {
         if(keystore.containsAlias(RSA_ALIAS)) {
             val entry = keystore.getEntry(RSA_ALIAS, null) as KeyStore.PrivateKeyEntry
-            rsaKey = KeyPair(entry.certificate.publicKey, entry.privateKey)
+            return KeyPair(entry.certificate.publicKey, entry.privateKey)
         } else {
-            val keyGen = KeyPairGenerator.getInstance(KeyProperties.KEY_ALGORITHM_RSA, ANDROID_KEYSTORE)
+            val keyGen = KeyPairGenerator.getInstance(
+                KeyProperties.KEY_ALGORITHM_RSA,
+                ANDROID_KEYSTORE
+            )
             val spec = KeyGenParameterSpec.Builder(
                 RSA_ALIAS,
                 KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
@@ -49,17 +49,8 @@ object RSAUtils {
             }.build()
 
             keyGen.initialize(spec)
-            rsaKey = keyGen.generateKeyPair()
+            return keyGen.generateKeyPair() // 생성과 동시에 KeyStore 에 저장됨
         }
-    }
-
-    /**
-     * RSA 키의 공개키 리턴
-     */
-    fun getRsaPublicKey(): ByteArray {
-        if(rsaKey == null) throw CryptoException("RSA 키가 생성되지 않았습니다.")
-
-        return rsaKey!!.public.encoded
     }
 
     /**
@@ -67,11 +58,9 @@ object RSAUtils {
      * @param 암호화하고자 하는 ByteArray
      * @return 암호화된 값
      */
-    fun encrypt(origin: ByteArray): ByteArray {
-        if(rsaKey == null) throw CryptoException("RSA 키가 생성되지 않았습니다.")
-
+    fun encrypt(publicKey: PublicKey, origin: ByteArray): ByteArray {
         val cipher = Cipher.getInstance(RSA_TRANSFORMATION)
-        cipher.init(Cipher.ENCRYPT_MODE, rsaKey!!.public)
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey)
         return cipher.doFinal(origin)
     }
 
@@ -80,11 +69,9 @@ object RSAUtils {
      * @param 암호화하고자 하는 문자열
      * @return 암호화된 값
      */
-    fun encrypt(origin: String): ByteArray {
-        if(rsaKey == null) throw CryptoException("RSA 키가 생성되지 않았습니다.")
-
+    fun encrypt(publicKey: PublicKey, origin: String): ByteArray {
         val cipher = Cipher.getInstance(RSA_TRANSFORMATION)
-        cipher.init(Cipher.ENCRYPT_MODE, rsaKey!!.public)
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey)
         return cipher.doFinal(origin.toByteArray(Charsets.UTF_8))
     }
 
@@ -93,11 +80,9 @@ object RSAUtils {
      * @param encryptedValue 암호화된 값
      * @return 복호화된 ByteArray
      */
-    fun decryptByteArray(encryptedValue: ByteArray) : ByteArray {
-        if(rsaKey == null) throw CryptoException("RSA 키가 생성되지 않았습니다.")
-
+    fun decryptByteArray(privateKey: PrivateKey, encryptedValue: ByteArray) : ByteArray {
         val cipher = Cipher.getInstance(RSA_TRANSFORMATION)
-        cipher.init(Cipher.DECRYPT_MODE, rsaKey!!.private)
+        cipher.init(Cipher.DECRYPT_MODE, privateKey)
         return cipher.doFinal(encryptedValue)
     }
 
@@ -106,11 +91,9 @@ object RSAUtils {
      * @param encryptedValue 암호화된 값
      * @return 복호화된 문자열
      */
-    fun decrypt(encryptedValue: ByteArray) : String {
-        if(rsaKey == null) throw CryptoException("RSA 키가 생성되지 않았습니다.")
-
+    fun decrypt(privateKey: PrivateKey, encryptedValue: ByteArray) : String {
         val cipher = Cipher.getInstance(RSA_TRANSFORMATION)
-        cipher.init(Cipher.DECRYPT_MODE, rsaKey!!.private)
+        cipher.init(Cipher.DECRYPT_MODE, privateKey)
         val decryptValue = cipher.doFinal(encryptedValue)
         return String(decryptValue, Charsets.UTF_8)
     }
